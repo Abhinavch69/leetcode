368420e7fe462771651604b4b5f398734f1d2345
Abhinav Chauhan <abhinavchauhan2526@gmail.com>
2025-03-20 16:08:24 +0530

    Time: 1 ms (94.20%) | Memory: 46 MB (12.30%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -1,36 +1,37 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-    // Remove leading nodes with value 'val'
-    while (head != null && head.val == val) {
-        head = head.next;
-    }
-
-    // If the list is empty after removing leading nodes
-    if (head == null) return null;
-
-    ListNode prev = head;
-    ListNode temp = head.next;
-
-    while (temp != null) {
-        if (temp.val == val) {
-            prev.next = temp.next; // Remove the node
-        } else {
-            prev = temp; // Move prev only when temp is not removed
+        // Remove leading nodes with value 'val'
+        while (head != null && head.val == val) {
+            head = head.next;
         }
-        temp = temp.next; // Always move temp
-    }
 
-    return head;
-}
+        // If the list is empty after removing leading nodes
+        if (head == null)
+            return null;
+
+        ListNode prev = head;
+        ListNode temp = head.next;
+
+        while (temp != null) {
+            if (temp.val == val) {
+                prev.next = temp.next; // Remove the node
+            } else {
+                prev = temp; // Move prev only when temp is not removed
+            }
+            temp = temp.next; // Always move temp
+        }
+
+        return head;
+    }
 
 }
